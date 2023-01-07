3a11f568988d31c0fe4998b1e22df399a76e507a
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-07 22:23:18 +0600

    done showing country name

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -14,7 +14,11 @@ const Countries = () => {
         <div>
             <h1>Hello from Countries: {countries.length}</h1>
             {
-                countries.map(country => <Country name={country.name.common}></Country>)
+                countries.map(country => <Country
+                    name={country.name.common}
+                    population={country.population}
+                    area={country.area}
+                ></Country>)
             }
         </div>
     );
