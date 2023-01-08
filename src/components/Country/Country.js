05785a88fa9735e13676a8ef89d51488aa48f19b
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-08 09:46:20 +0600

    shown flag and added style on the flags

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Country.css';
 
 const Country = (props) => {
-    const { name, population, region, area } = props.country;
+    const { name, population, region, area, flags } = props.country;
     return (
         <div className='country'>
             <h3>Name: {name.common}</h3>
+            <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p>Region: {region}</p>
