8c44602a98fef78a69ba2502861a170f7032412f
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-08 09:58:48 +0600

    added an unique key and done some changes

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -16,7 +16,7 @@ const Countries = () => {
             <h1>Hello from Countries: {countries.length}</h1>
             <div className='countries-container'>
                 {
-                    countries.map(country => <Country country={country}></Country>)
+                    countries.map(country => <Country country={country} key={country.cca3}></Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,9 +3,10 @@ import './Country.css';
 
 const Country = (props) => {
     const { name, population, region, area, flags } = props.country;
+    // console.log(props.country);
     return (
         <div className='country'>
-            <h3>Name: {name.common}</h3>
+            <h3>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
